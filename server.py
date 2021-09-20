e8c355b37612ccc8dee44fc63401c1bbc97d7d93
h0dah <h.elbadrawe@gmail.com>
2021-09-20 22:25:22 +0200

    add the CRUD endopoints

--- server.py
+++ server.py
@@ -1,7 +1,45 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+db = mysql.connector.connect(host=os.getenv("db_host"),
+                     user=os.getenv("db_user"),
+                     passwd=os.getenv("db_passwd"),
+                     db="customers")  
 
-@app.route("/")
-def create():
-    return("okay")
+@app.route('/customer/<int:customer_id>', methods = ['GET'])
+def get_customer(customer_id):
+    cursor = db.cursor()
+    cursor.execute(f"select name from customer where id = {customer_id}")
+    customer = cursor.fetchall()[0]
+    return make_response(jsonify({"customer": customer[0]}))
+
+@app.route('/customer/add', methods = ['POST'])
+def add_customer():
+    request_data = request.get_json()
+    name = request_data['name']
+    cursor = db.cursor()
+    cursor.execute(f"INSERT INTO customer (name) VALUES ('{name}')")
+    db.commit()
+    return make_response(jsonify({}), 201)
+
+@app.route('/customer/<int:customer_id>/update', methods = ['POST'])
+def update_customer(customer_id):
+    request_data = request.get_json()
+    name = request_data['name']
+    cursor = db.cursor()
+    cursor.execute(f"UPDATE customer SET name = '{name}' where id = {customer_id}")
+    db.commit()
+    return make_response(jsonify({}), 204)
+
+@app.route('/customer/<int:customer_id>/delete', methods = ["DELETE"])
+def delete_customer(customer_id):
+    cursor = db.cursor()
+    cursor.execute(f"DELETE FROM customer where id = {customer_id}")
+    db.commit()
+    return make_response(jsonify({}), 204)
+
